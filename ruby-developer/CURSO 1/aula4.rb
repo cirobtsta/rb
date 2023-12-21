5d38f5d9284a27e2bb9036b186ceefee3ca6f6d0
root <root@N5>
2023-12-21 07:19:46 -0300

    entras e saídas

---
+++ ruby-developer/CURSO 1/aula4.rb
@@ -0,0 +1,16 @@
+# 1. saída de dados na tela --> aparecer um comando na tela
+# usuário digite seu nome
+print "Digite seu nome: "
+
+
+# 2. programa permita que o usuário preencha o que foi pedido
+# gets.chomp
+ nome = gets.chomp      
+
+# 3. saída final
+# para chamar um dado #{variável}
+puts "Oi #{nome} !"
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Oi #{nome} #{sobrenome}"
