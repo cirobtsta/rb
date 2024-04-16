2c81492ab62076b02a58deb91367691e73c94c3d
ciro batista <cirobtsta@gmail.com>
2024-04-16 07:16:09 -0300

    loop

---
+++ formacao-ruby-developer/estruturas-de-controle/do-while.rb
@@ -0,0 +1,17 @@
+#criar um contador
+#precisar de uma variável
+
+count = 0
+
+
+#loop de exceução
+
+loop do
+    puts count
+    #condição --> Se (IF) contador não for igual a 150
+    if count == 150
+        break
+    end
+    #incremento  --> somar 1
+    count += 1
+end
---
+++ formacao-ruby-developer/estruturas-de-controle/time.rb
@@ -0,0 +1,17 @@
+#6.times {puts "Oi"}
+
+#6.times {print "Oi "}
+
+#4.times do
+#    puts "oi"
+#end
+
+#4.times do
+#    print "oi "
+#end
+
+20.times do |contador|
+    puts "#{contador}"
+end
+
+
