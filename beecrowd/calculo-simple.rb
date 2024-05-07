2ed4a68a8e2f993a3fdae7c610038fed56042db2
Ciro Batista <cirobtsta@gmail.com>
2024-05-07 03:23:35 -0300

    calculo-simples

--- beecrowd/calculo-simple.rb
+++
@@ -1,1 +0,0 @@
-peca1, quantidade1, valor1 = gets.split.map(&:to_f)
---
+++ beecrowd/calculo-simples.rb
@@ -0,0 +1,30 @@
+
+#1010 CALCULO
+
+# Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+# Entrada
+# O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+# Saída
+# A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+
+
+# Receber entrada do usuário
+
+puts ("Inserir: cod1 num1 vr1")
+cod1, num1 = gets.split.map(&:to_i)
+vr1 = gets.to_f
+
+puts ("Inserir: cod2 num2 vr2")
+cod2, num2, = gets.split.map(&:to_i)
+vr1 = gets.to_f
+
+total1 = num1
+
+# total = (num1 * vr1) + (num2 * vr2)
+
+print cod1, " ", num1, " ", vr1, "\n"
+print cod2, " ", num2, " ", vr2, "\n"
+
+# printf "VALOR A PAGAR: R$ %.2f\n", total
